51a379d0bd14da8e2515b3076c4c2efc031b9786
Nick H <nhuntington11@gmail.com>
2018-06-23 10:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const list = document.getElementsByClassName('ranked-list').querySelectorAll('<li>')
+  const list = document.querySelectorAll('')
   
   for (var i = 0; i < list.length; i++) {
     
